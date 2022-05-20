7549946311a000aba94eb43dbaf6250fac24f2cf
Pedro-Porras-Luyo <101282363+Pedro-Porras-Luyo@users.noreply.github.com>
2022-05-19 23:22:55 -0500

    preversion

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('load',() => {
+window.addEventListener('load', () => {
     const contenedor_loader = document.querySelector('.contenedorr')
     contenedor_loader.style.opacity = 0
     contenedor_loader.style.visibility = 'hidden'
