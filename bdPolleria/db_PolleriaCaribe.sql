8eb26c1d7298031a667da7cc74204d6e81a91895
PedroPL <101282363+PedroPorrasLuyo@users.noreply.github.com>
2022-06-21 19:27:45 +0200

    Update db_PolleriaCaribe.sql

--- bdPolleria/db_PolleriaCaribe.sql
+++ bdPolleria/db_PolleriaCaribe.sql
@@ -111,16 +111,26 @@ ALTER TABLE VENTA ADD CONSTRAINT VENTA_EMPLEADO FOREIGN KEY VENTA_EMPLEADO (CODE
 
 /*---------------------------------- CREACION DE TABLA PEDIDO ------------------------------*/
 
-/* Crear tabla: PEDIDO */
-CREATE TABLE PEDIDO (
-    IDPED int AUTO_INCREMENT,
-    USERPED varchar(100) NOT NULL,
-    GMLUSPED varchar(100) NOT NULL,
-    CELUSPED char(9) NOT NULL,
-    FOODPED varchar(80) NOT NULL,
-    MSGPED varchar(600) NOT NULL,
-    CONSTRAINT PEDIDO_pk PRIMARY KEY (IDPED)
+CREATE TABLE Pedido
+(
+     IDPED int AUTO_INCREMENT,
+     USERPED varchar(80),
+     EMAUSPED varchar(80),
+     CELUSPED char(9),
+     FOODPED varchar(80),
+     MSGPED   varchar(250),
+     PRIMARY KEY (IDPED)
 );
+
+/* Insertar registros */
+INSERT INTO PEDIDO
+(USERPED, EMAUSPED, CELUSPED, FOODPED, MSGPED)
+VALUES
+('José Ramírez', 'jose.ramirez@outlook.com', '974815236', 'Arroz con Pollo', 'Por favor enviar a la dirección Av. Miraflores 253, San Vicente de Cañete'),
+('Ana Guerra Solano', 'ana.guerra@gmail.com', '981526321', 'Sopa Seca de Gallina', 'Necesito 4 platos');
+
+/* Listar los registros de la tabla PEDIDO */
+SELECT * FROM PEDIDO;
 
 /* Insertar registros */
 INSERT INTO PEDIDO
